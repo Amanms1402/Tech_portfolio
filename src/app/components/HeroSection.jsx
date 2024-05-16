fefc1c94ed70d8a8765533e3f2eaddb512cc71dd
Amanms1402 <119517551+Amanms1402@users.noreply.github.com>
2024-05-16 22:36:01 +0530

    first commit

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 
 const HeroSection = () => {
   return (
-    <section className="lg:py-16">
+    <section>
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
@@ -64,13 +64,14 @@ const HeroSection = () => {
           <br/>
           <br />
           <br />
+          <br />
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
               src="/images/hero-image.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={300}
-              height={300}
+              width={320}
+              height={320}
             />
           </div>
         </motion.div>
